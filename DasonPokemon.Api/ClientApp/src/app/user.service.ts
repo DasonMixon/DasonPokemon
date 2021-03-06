0410ecd00e631c103859c37840087c3c935f7e2e
Mason Dixon <dasonmixon@hotmail.com>
2021-03-06 02:57:07 -0500

    Other user service stuff

--- DasonPokemon.Api/ClientApp/src/app/user.service.ts
+++ DasonPokemon.Api/ClientApp/src/app/user.service.ts
@@ -1,17 +1,30 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LocalStorageService } from './local-storage.service';
-import { Observable } from 'rxjs'
+import { Observable, of } from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+  private userId : string | null = null;
+
   constructor(private http : HttpClient, private storage : LocalStorageService) {
+    const storageUser = this.storage.get('user') as User;
+    if (storageUser !== null && storageUser !== undefined) {
+      this.userId = storageUser.id;
+    }
   }
 
-  public getUser(id : string) : Observable<User> {
+  public setUserId(id : string) {
+    this.userId = id;
+  }
+
+  public getUser(id : string | null = this.userId) : Observable<User> {
+    if (id === null)
+      return of<User>();
+    
     return this.storage.getOrFetch('user', this.http.get<User>(`api/user/${id}`));
   }
 }
